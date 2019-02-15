418158bf917f788d334f1c427a20fc22867b5819
Cameron Bernhardt <cambernhardt@me.com>
2019-02-15 17:03:57 -0500

    Add pulsating demo

---
+++ pulsating/pulsating.ino
@@ -0,0 +1,31 @@
+#include "FastLED.h"
+#define NUM_LEDS 300
+
+CRGB leds[NUM_LEDS];
+
+void setup()
+{
+    FastLED.addLeds<WS2812B, 9, GRB>(leds, NUM_LEDS);
+    for (int i = 0; i < NUM_LEDS; i++)
+    {
+        leds[i] = CRGB::White;
+    }
+}
+void loop()
+{
+    for (int i = 0; i <= 255; i++)
+    {
+        FastLED.setBrightness(i);
+        FastLED.show();
+    }
+
+    FastLED.show();
+    delay(500);
+
+    for (int i = 255; i >= 0; i--)
+    {
+        FastLED.setBrightness(i);
+        FastLED.show();
+    }
+    delay(500);
+}

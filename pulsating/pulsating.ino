26c6df4debadeaf9101313653f3283e49f3f6003
Cameron Bernhardt <cambernhardt@me.com>
2019-02-16 00:35:34 -0500

    Color option

--- pulsating/pulsating.ino
+++ pulsating/pulsating.ino
@@ -3,18 +3,14 @@
 
 CRGB leds[NUM_LEDS];
 
-void setup()
-{
+void setup() {
     FastLED.addLeds<WS2812B, 9, GRB>(leds, NUM_LEDS);
-    for (int i = 0; i < NUM_LEDS; i++)
-    {
+    for (int i = 0; i < NUM_LEDS; i++) {
         leds[i] = CRGB::White;
     }
 }
-void loop()
-{
-    for (int i = 0; i <= 255; i++)
-    {
+void loop() {
+    for (int i = 0; i <= 255; i++) {
         FastLED.setBrightness(i);
         FastLED.show();
     }
@@ -22,8 +18,7 @@ void loop()
     FastLED.show();
     delay(500);
 
-    for (int i = 255; i >= 0; i--)
-    {
+    for (int i = 255; i >= 0; i--) {
         FastLED.setBrightness(i);
         FastLED.show();
     }

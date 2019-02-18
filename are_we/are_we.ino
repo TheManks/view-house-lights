e61c705a4763ef4784a3f700e83cb2ecaeca96c0
pi <pi>
2019-02-18 16:22:04 -0500

    Larry: Cleaned up old code in arduino sketch, got rid of unimportant files

--- are_we/are_we.ino
+++ are_we/are_we.ino
@@ -5,35 +5,6 @@
 CRGB leds[NUM_LEDS];
 
 
-
-
-
-
-//char buf[80];
-
-/*int readline(int readch, char *buffer, int len) {
-    static int pos = 0;
-    int rpos;
-
-    if (readch > 0) {
-        switch (readch) {
-            case '\r': // Ignore CR
-                break;
-            case '\n': // Return on new-line
-                rpos = pos;
-                pos = 0;  // Reset position index ready for next time
-                return rpos;
-            default:
-                if (pos < len-1) {
-                    buffer[pos++] = readch;
-                    buffer[pos] = 0;
-                }
-        }
-    }
-    return 0;
-}*/
-
-
 CmdMessenger cmdMessenger = CmdMessenger(Serial);
 enum {
     setLed,
@@ -61,48 +32,7 @@ void loop() {
     cmdMessenger.feedinSerialData();
 
 
-    /*if(readline(Serial.read(), buf, 80) > 0) {
-	Serial.print("You entered: >");
-        Serial.print(buf);
-        Serial.println("<");
-        //i = atoi(buf);
-        //FastLED.setBrightness(i);
-        //FastLED.show();
-    }
-    FastLED.show();*/
 }
 
 
-
-
-
-
-
-/*  int data = 0;
-    Serial.setTimeout(0);
-    while (!Serial.available()) {} // wait
-    while (Serial.available()) {
-        data = Serial.parseInt();	
-        FastLED.setBrightness((int)data);
-	FastLED.show();
-	Serial.print("Yo I got:");
-	Serial.println(data);
-	Serial.print("\n");
-*/
-
-/* 
-   for (int i = 0; i <= 255; i++) {
-        FastLED.setBrightness(i);
-        FastLED.show();
-    }
-
-    FastLED.show();
-    delay(500);
-
-    for (int i = 255; i >= 0; i--) {
-        FastLED.setBrightness(i);
-        FastLED.show();
-    }
-    delay(500);
-}*/
 
